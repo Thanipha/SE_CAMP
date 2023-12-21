0465fafee6851895aab06305ecf2007276506d91
Thanipha <65160325@go.buu.ac.th>
2023-12-21 18:35:10 +0700

    ีรีร

--- index.php
+++ index.php
@@ -70,14 +70,14 @@
     $arry2[] = 3;
     $arry2[] = "Hello";
     $arry2['SE'] = "World";
-    
+
     ?><?php print_r($arry);?>                               
     <pre><?php print_r($arry);?></pre>
     <pre><?php print_r($arry1);?></pre>
     <pre><?php print_r($arry2);?></pre>
 
     <?php for($i = 0; $i <count($arry2);$i++){ ?>
-    <h3><?php echo $arry2[$i];?></h3>
+    <h3><?php echo $arry2[$i]; หิวข้าวจัง?></h3>
     <?php } ?>
 
     <?php foreach($arry2 as $key => $value){ ?>
