3d2042c6f6b9392feea6c14f0f1dea671c74f877
Thanipha <65160325@go.buu.ac.th>
2023-12-25 19:34:28 +0700

    update laravel

---
+++ example-app/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/my-route', function () {
+    //echo "<h1> My Route Page</h1>";
+    //return view('')
+    //return viem('myroute');
+    $data = ['val_a' => 'Hello World!'];
+    $data['val_b'] = "Lalaval";
+    return view('myfolder.mypage',$data);
+});
+
+Route::post('/my-route',function(Request $req){
+    $data['myinput'] = $req -> input('myinput');
+    return view ('myroute',$data);
+});
+
--- form101.php
+++ form101.php
@@ -3,12 +3,14 @@
     <meta charset="UTF-8">
 </head>
 <body>
-    <form action = "paht" method = "post">
+    <form action = "" method = "post">
         <input type = "text" name = "my_val"/>
         <button type = "submit" > บันทึก </button>
     </form>
     <h1><?php 
-    print_r($_REQUEST);//รับได้หมด
+    //print_r($_POST['my_val']);
+    //print_r($_POST);
+   // print_r($_REQUEST);//รับได้หมด
     if(isset($POST['my_val'])) {
         echo $_POST['my_val'];
     } ?></h1>
