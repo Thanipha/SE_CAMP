36375528f1c911c86eeda0f66263f88ba1c68276
Thanipha <65160325@go.buu.ac.th>
2024-01-15 19:56:49 +0700

    up

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\C_titles;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,9 @@ Route::namespace('App\Http\Controllers')->group(function(){
     Route::post('/my-controller3-post', 'MyController@index');
 });
 Route::resource('/my-controller4', MyController::class);
+
+Route::resource('titles', C_titles::class);
+
 
 
 Route::get('/', function () {
